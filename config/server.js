08b74d39b7bcdadc4fbac72c04959786d6419706
VHALL <70254697+vhalllive@users.noreply.github.com>
2022-04-08 12:16:34 +0530

    Server url changed to EC2 ip

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://localhost:1337',
+  url: env("http://13.127.235.179:1337", "http://localhost:1337"),
 });
